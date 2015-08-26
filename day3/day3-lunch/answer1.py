736e1b7c3d999a4a077638f2c21b850a518229b6
Nelmari Ruiz <nelmari634@aol.com>
2015-08-26 12:32:53 -0400

    Day 3:Lunch exercise 1

--- day3/day3-lunch/answer1.py
+++ day3/day3-lunch/answer1.py
@@ -21,19 +21,16 @@ for sample in metadata [metadata["sex"] == "male"]["sample"]:
 replicates = pd.read_csv("/Users/cmdb/qbb2015/rawdata/replicates.csv")
 
 for columns in replicates["sample"]:
-    df_r = pd.read_table ("~/qbb2015/stringtie/"+sample+"/t_data.ctab")
+    df_r = pd.read_table ("~/qbb2015/stringtie/"+columns+"/t_data.ctab")
     roi_r = df_r["t_name"].str.contains("FBtr0331261")
     Sxl_r.append(df_r[roi_r]["FPKM"].values)
-    print "0"
-
-
-####   
+      
 plt. figure
 plt.plot(Sxl_f, label ="Female", color = "m")
 plt.plot(Sxl_m, label= "Male", color = "c")
 plt.plot(Sxl_r, label= "Replicates", color = "g")
 plt.legend()
-plt.axis([2,8,0,130])
+plt.axis([2,8,0,200])
 plt.xlabel("Developmental Stage")
 plt.ylabel("FPKM values")
 plt.savefig("time_course_fm.png")
